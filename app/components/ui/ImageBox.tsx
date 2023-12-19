b9b1585877412928ecf512b5d30fe13f6b7c6116
Santa0727 <santa0727@outlook.com>
2023-12-20 05:07:17 +0800

    finish: inspect logic

---
+++ app/components/ui/ImageBox.tsx
@@ -0,0 +1,91 @@
+import {
+  Image,
+  StyleProp,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+const options: ImagePicker.ImagePickerOptions = {
+  mediaTypes: ImagePicker.MediaTypeOptions.Images,
+  base64: true,
+};
+
+interface Props {
+  image?: string;
+  onChange?: (d: string) => void;
+  style?: StyleProp<ViewStyle>;
+}
+
+const ImageBox = ({ image, onChange, style }: Props) => {
+  const onOpenCamera = async () => {
+    const status = await ImagePicker.requestCameraPermissionsAsync();
+    if (!status.granted) {
+      alert('You have no camera access permission');
+      return;
+    }
+
+    const lunch = await ImagePicker.launchCameraAsync(options);
+
+    if (lunch?.canceled !== true && lunch.assets.length > 0) {
+      const data = lunch.assets[0];
+      let arr = data.uri.split('/');
+      const filename = arr[arr.length - 1];
+      arr = filename.split('.');
+      const ext = arr[arr.length - 1].toLowerCase();
+
+      if (ext !== 'png' && ext !== 'jpg' && ext !== 'jpeg') return;
+
+      const type =
+        ext === 'png'
+          ? 'image/png'
+          : ext === 'jpg'
+          ? 'image/jpg'
+          : 'image/jpeg';
+
+      if (onChange) {
+        onChange(`data:${type};base64,${data.base64}`);
+      }
+    }
+  };
+
+  return (
+    <View style={[styles.wrapper, style]}>
+      <TouchableOpacity style={styles.touch_img} onPress={onOpenCamera}>
+        <Image
+          resizeMode="cover"
+          style={styles.image}
+          source={
+            image
+              ? {
+                  uri: image,
+                }
+              : require('../../images/placeholder.jpg')
+          }
+          alt="Pickup image"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  touch_img: {
+    borderColor: 'lightgrey',
+    borderWidth: 1,
+    borderRadius: 3,
+    height: 150,
+    minHeight: 150,
+    minWidth: 150,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+  },
+});
+
+export default ImageBox;
--- app/screens/InspectEntryScreen.tsx
+++ app/screens/InspectEntryScreen.tsx
@@ -1,245 +1,276 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import MainContainer from '../components/container/MainContainer';
 import { FontAwesome } from '@expo/vector-icons';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Checkbox from '../components/ui/Checkbox';
 import RadioSelect from '../components/ui/RadioSelect';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { InspectStackParamList } from '../navigation/AppStackParams';
+import { sendRequest } from '../config/compose';
+import { useFocusEffect } from '@react-navigation/native';
+import HTMLView from 'react-native-htmlview';
+import ImageBox from '../components/ui/ImageBox';
 
-const StepOneView = () => (
-  <View style={{ paddingHorizontal: 10 }}>
-    <Text style={{ fontSize: 24, fontWeight: '600' }}>Read before start</Text>
-    <Text style={{ marginVertical: 10, fontSize: 19 }}>
-      {
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan accumsan lorem, nec lobortis est tincidunt vel. Donec lobortis porttitor dapibus. Praesent pretium suscipit nulla. Fusce mollis hendrerit risus vel tincidunt. Quisque mattis ac lacus non posuere. Morbi facilisis fringilla lorem ac consequat. Ut vitae consequat tortor, sit amet laoreet felis. Nunc eleifend, nunc eget laoreet faucibus, erat mi fringilla'
-      }
-    </Text>
-    <View style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 10 }}>
-      <Text style={{ fontSize: 19 }}>{'1. '}</Text>
-      <Text style={{ fontSize: 19 }}>
-        {
-          'Quisque auctor leo nec eros faucibus tincidunt et et tellus. In ut ultrices nibh'
-        }
-      </Text>
-    </View>
-    <View style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 10 }}>
-      <Text style={{ fontSize: 19 }}>{'2. '}</Text>
-      <Text style={{ fontSize: 19 }}>
-        {
-          'Donec neque ex, eleifend ut ipsum vel, finibus malesuada sapien. Quisque non dui sit amet sapien porta fringilla.'
-        }
-      </Text>
-    </View>
-    <View style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 10 }}>
-      <Text style={{ fontSize: 19 }}>{'3. '}</Text>
-      <Text style={{ fontSize: 19 }}>
-        {
-          'Aenean enim nisl, venenatis non enim bibendum, accumsan pellentesque eros. Nulla rhoncus tortor metus, ac rhoncus lacus cursus et.'
-        }
-      </Text>
-    </View>
-  </View>
-);
+interface IStep {
+  id: string;
+  name: string;
+  text: string;
+  type: 'multipleimage' | 'checkbox' | 'radio';
+  options: Array<{ id: string; name: string }>;
+}
 
-const StepTwoView = () => (
-  <View>
-    <View
-      style={{
-        flexDirection: 'row',
-        borderColor: '#d1d1d1',
-        borderBottomWidth: 1,
-        marginHorizontal: 20,
-        alignItems: 'center',
-        paddingBottom: 10,
-      }}>
-      <FontAwesome name="home" size={30} />
-      <View style={{ marginLeft: 10 }}>
-        <Text style={{ fontSize: 22, fontWeight: '500' }}>Location</Text>
-        <Text style={{ fontSize: 18, fontWeight: '400' }}>Freezer</Text>
+interface IEntry {
+  first: string;
+  steps: IStep[];
+  last: string;
+}
+
+interface FormProps {
+  form: any;
+  setForm: (d: any) => void;
+  step: IStep;
+}
+
+const StepForm = ({ form, setForm, step }: FormProps) => {
+  const changeImage = (id: string, img: string) => {
+    setForm({ ...form, [id]: img });
+  };
+  const toggleCheck = (id: string, c: boolean) => {
+    setForm({ ...form, [id]: c });
+  };
+  const selectRadio = (c: string) => {
+    const v = step.options.find((x) => !!form[x.id]);
+    let tmp = { ...form };
+    if (v) {
+      tmp[v.id] = false;
+    }
+    tmp[c] = true;
+    setForm(tmp);
+  };
+
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          borderColor: '#d1d1d1',
+          borderBottomWidth: 1,
+          marginHorizontal: 20,
+          alignItems: 'center',
+          paddingBottom: 10,
+        }}>
+        <FontAwesome name="home" size={30} />
+        <View style={{ marginLeft: 10 }}>
+          <Text style={{ fontSize: 22, fontWeight: '500' }}>Location</Text>
+          <Text style={{ fontSize: 18, fontWeight: '400' }}>{step.name}</Text>
+        </View>
       </View>
-    </View>
-    <Text style={{ fontSize: 20, marginVertical: 20, marginHorizontal: 10 }}>
-      {
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan accumsan lorem, nec lobortis est tincidunt vel.'
-      }
-    </Text>
-    <View style={cardStyle.card}>
-      <Image
-        source={require('../images/demo-school-image.png')}
-        style={cardStyle.img}
+      <HTMLView
+        style={{ marginVertical: 20, marginHorizontal: 10 }}
+        value={step.text}
       />
-      <View style={cardStyle.name_view}>
-        <Text style={cardStyle.time}>{'28-Oct 10:24 am'}</Text>
-        <Text style={cardStyle.name}>{'Picture of inside of freezer'}</Text>
-      </View>
+      {step.type === 'multipleimage' ? (
+        step.options.map((x) => (
+          <View key={x.id} style={cardStyle.card}>
+            <ImageBox
+              image={form[x.id] ?? undefined}
+              onChange={(m) => changeImage(x.id, m)}
+            />
+            <View style={cardStyle.name_view}>
+              <Text style={cardStyle.time}>{x.name}</Text>
+              <Text style={cardStyle.name}>{''}</Text>
+            </View>
+          </View>
+        ))
+      ) : step.type === 'checkbox' ? (
+        <View style={{ marginVertical: 10, paddingHorizontal: 10 }}>
+          <Text style={{ fontSize: 21, fontWeight: '600', marginBottom: 5 }}>
+            {'Did you check this'}
+          </Text>
+          {step.options.map((x) => (
+            <Checkbox
+              key={x.id}
+              value={form[x.id] ?? false}
+              label={x.name}
+              onChange={(c) => toggleCheck(x.id, c)}
+            />
+          ))}
+        </View>
+      ) : (
+        <View style={{ marginVertical: 10, paddingHorizontal: 10 }}>
+          <Text style={{ fontSize: 21, fontWeight: '600', marginBottom: 5 }}>
+            {'Which one it is'}
+          </Text>
+          <RadioSelect
+            options={step.options.map((x) => ({ id: x.id, label: x.name }))}
+            value={step.options.find((x) => !!form[x.id])?.id}
+            onChange={(c) => selectRadio(c.toString())}
+          />
+        </View>
+      )}
     </View>
-    <View style={cardStyle.card}>
-      <Image
-        source={require('../images/demo-school-image.png')}
-        style={cardStyle.img}
-      />
-      <View style={cardStyle.name_view}>
-        <Text style={cardStyle.time}>{'28-Oct 10:24 am'}</Text>
-        <Text style={cardStyle.name}>{'Picture of inside of freezer'}</Text>
-      </View>
-    </View>
-    <View style={cardStyle.card}>
-      <Image
-        source={require('../images/demo-school-image.png')}
-        style={cardStyle.img}
-      />
-      <View style={cardStyle.name_view}>
-        <Text style={cardStyle.time}>{'28-Oct 10:24 am'}</Text>
-        <Text style={cardStyle.name}>{'Picture of inside of freezer'}</Text>
-      </View>
-    </View>
-  </View>
-);
+  );
+};
 
-const radioOptions = [
-  { id: 1, label: 'This one' },
-  { id: 2, label: 'Or this one' },
-];
-
-const StepThreeView = () => (
-  <View>
-    <View
-      style={{
-        flexDirection: 'row',
-        borderColor: '#d1d1d1',
-        borderBottomWidth: 1,
-        marginHorizontal: 20,
-        alignItems: 'center',
-        paddingBottom: 10,
-      }}>
-      <FontAwesome name="home" size={30} />
-      <View style={{ marginLeft: 10 }}>
-        <Text style={{ fontSize: 22, fontWeight: '500' }}>Location</Text>
-        <Text style={{ fontSize: 18, fontWeight: '400' }}>Freezer</Text>
-      </View>
-    </View>
-    <Text style={{ fontSize: 20, marginVertical: 20, marginHorizontal: 10 }}>
-      {'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-    </Text>
-    <View style={{ marginVertical: 10, paddingHorizontal: 10 }}>
-      <Text style={{ fontSize: 21, fontWeight: '600', marginBottom: 5 }}>
-        {'Did you check this'}
-      </Text>
-      <Checkbox value={true} label="I confirm that this is checked" />
-      <Checkbox value={false} label="I confirm that this is checked" />
-    </View>
-    <View style={{ marginVertical: 10, paddingHorizontal: 10 }}>
-      <Text style={{ fontSize: 21, fontWeight: '600', marginBottom: 5 }}>
-        {'Which one it is'}
-      </Text>
-      <RadioSelect options={radioOptions} value={1} />
-    </View>
-  </View>
-);
-
-const StepFourView = () => (
-  <View style={{ paddingHorizontal: 10 }}>
-    <Text style={{ fontSize: 24, fontWeight: '600' }}>
-      {'Thank you for your submitting report'}
-    </Text>
-    <Text style={{ marginVertical: 10, fontSize: 19 }}>
-      {
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan accumsan lorem, nec lobortis est tincidunt vel. Donec lobortis porttitor dapibus. Praesent pretium suscipit nulla. Fusce mollis hendrerit risus vel tincidunt. Quisque mattis ac lacus non posuere. Morbi facilisis fringilla lorem ac consequat. Ut vitae consequat tortor, sit amet laoreet felis. Nunc eleifend, nunc eget laoreet faucibus, erat mi fringilla'
-      }
-    </Text>
-    <View style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 10 }}>
-      <Text style={{ fontSize: 19 }}>{'1. '}</Text>
-      <Text style={{ fontSize: 19 }}>
-        {
-          'Quisque auctor leo nec eros faucibus tincidunt et et tellus. In ut ultrices nibh'
-        }
-      </Text>
-    </View>
-    <View style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 10 }}>
-      <Text style={{ fontSize: 19 }}>{'2. '}</Text>
-      <Text style={{ fontSize: 19 }}>
-        {
-          'Donec neque ex, eleifend ut ipsum vel, finibus malesuada sapien. Quisque non dui sit amet sapien porta fringilla.'
-        }
-      </Text>
-    </View>
-    <View style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 10 }}>
-      <Text style={{ fontSize: 19 }}>{'3. '}</Text>
-      <Text style={{ fontSize: 19 }}>
-        {
-          'Aenean enim nisl, venenatis non enim bibendum, accumsan pellentesque eros. Nulla rhoncus tortor metus, ac rhoncus lacus cursus et.'
-        }
-      </Text>
-    </View>
-  </View>
+const StepSector = ({ isActive }: { isActive: boolean }) => (
+  <>
+    <View style={isActive ? styles.step_line : styles.inactive_line} />
+    <View style={isActive ? styles.step_circle : styles.inactive_circle} />
+  </>
 );
 
 type Props = NativeStackScreenProps<InspectStackParamList, 'InspectEntry'>;
 
-const InspectEntryScreen = ({ navigation }: Props) => {
-  const [step, setStep] = useState(1);
+const InspectEntryScreen = ({ navigation, route }: Props) => {
+  const inspectID = route.params.inspectID;
 
-  const nextClick = () => {
-    if (step >= 4) {
+  const [step, setStep] = useState(0);
+  const [entry, setEntry] = useState<IEntry>();
+  const [form, setForm] = useState<any>({});
+  const [disabled, setDisabled] = useState(false);
+
+  const validForm = () => {
+    if (!entry) return null;
+    let result: any = { inspection_id: inspectID };
+    for (let s of entry.steps) {
+      if (s.type === 'multipleimage') {
+        for (let o of s.options) {
+          if (!form[o.id]) {
+            alert(`Please pick image for ${o.name}`);
+            return null;
+          }
+          result[o.id] = form[o.id];
+        }
+      } else {
+        for (let o of s.options) {
+          result[o.id] = form[o.id] ?? false;
+        }
+      }
+    }
+    return result;
+  };
+
+  const nextClick = async () => {
+    if (!entry) return;
+    if (step === entry.steps.length) {
+      const data = validForm();
+      if (!data) return;
+
+      setDisabled(true);
+      const res = await sendRequest(
+        'api/member/inspections/submit',
+        data,
+        'POST',
+      );
+      setDisabled(false);
+      if (!res.status) {
+        alert(res.message ?? 'Server error');
+      } else {
+        setStep(step + 1);
+      }
+    } else if (step > entry.steps.length) {
       navigation.goBack();
     } else {
       setStep(step + 1);
     }
   };
 
+  const loadData = useCallback(() => {
+    sendRequest(`api/member/inspections/${inspectID}`, {}, 'GET').then(
+      (res) => {
+        if (res.status) {
+          setEntry(res.data);
+        } else {
+          alert(res.message ?? 'Server error');
+        }
+      },
+    );
+  }, [inspectID]);
+
+  useFocusEffect(loadData);
+
   return (
     <MainContainer style={{ padding: 5 }}>
-      {step === 1 ? (
-        <StepOneView />
-      ) : step === 2 ? (
-        <StepTwoView />
-      ) : step === 3 ? (
-        <StepThreeView />
+      {entry ? (
+        <>
+          <View style={{ padding: 10, minHeight: 300 }}>
+            {step === 0 ? (
+              <View>
+                <Text style={{ fontSize: 24, fontWeight: '600' }}>
+                  Read before start
+                </Text>
+                <HTMLView style={{ marginTop: 10 }} value={entry.first} />
+              </View>
+            ) : step > entry.steps.length ? (
+              <View>
+                <Text style={{ fontSize: 24, fontWeight: '600' }}>
+                  {'Thank you for your submitting report'}
+                </Text>
+                <HTMLView style={{ marginTop: 10 }} value={entry.last} />
+              </View>
+            ) : (
+              <StepForm
+                form={form}
+                setForm={setForm}
+                step={entry.steps[step - 1]}
+              />
+            )}
+          </View>
+          <View style={styles.steps_view}>
+            <View style={styles.steps_btn}>
+              {step > 0 && step <= entry.steps.length ? (
+                <TouchableOpacity
+                  style={[styles.next_btn, disabled ? styles.disabled : {}]}
+                  onPress={() => setStep(Math.max(0, step - 1))}
+                  disabled={disabled}>
+                  <Text style={styles.next_txt}>Previous</Text>
+                </TouchableOpacity>
+              ) : (
+                <View />
+              )}
+              <TouchableOpacity
+                style={[styles.next_btn, disabled ? styles.disabled : {}]}
+                onPress={nextClick}
+                disabled={disabled}>
+                <Text style={styles.next_txt}>
+                  {step === entry.steps.length
+                    ? 'Submit'
+                    : step === entry.steps.length + 1
+                    ? 'Finish'
+                    : 'Next'}
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.steps_bar}>
+              <View style={styles.step_circle} />
+              {Array.from(
+                { length: entry.steps.length + 1 },
+                (_, id) => id + 1,
+              ).map((x) => (
+                <StepSector key={x} isActive={step >= x} />
+              ))}
+            </View>
+          </View>
+        </>
       ) : (
-        <StepFourView />
+        <View style={{ paddingTop: '50%' }}>
+          <ActivityIndicator size="large" color={'black'} />
+        </View>
       )}
-      <View style={styles.steps_view}>
-        <View style={styles.steps_btn}>
-          {step > 1 ? (
-            <TouchableOpacity
-              style={styles.next_btn}
-              onPress={() => setStep(Math.max(1, step - 1))}>
-              <Text style={styles.next_txt}>Previous</Text>
-            </TouchableOpacity>
-          ) : (
-            <View />
-          )}
-          <TouchableOpacity style={styles.next_btn} onPress={nextClick}>
-            <Text style={styles.next_txt}>
-              {step === 4 ? 'Finish' : 'Next'}
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.steps_bar}>
-          <View style={styles.step_circle} />
-          <View style={step > 1 ? styles.step_line : styles.inactive_line} />
-          <View
-            style={step > 1 ? styles.step_circle : styles.inactive_circle}
-          />
-          <View style={step > 2 ? styles.step_line : styles.inactive_line} />
-          <View
-            style={step > 2 ? styles.step_circle : styles.inactive_circle}
-          />
-          <View style={step > 3 ? styles.step_line : styles.inactive_line} />
-          <View
-            style={step > 3 ? styles.step_circle : styles.inactive_circle}
-          />
-        </View>
-      </View>
     </MainContainer>
   );
 };
 
 const styles = StyleSheet.create({
   steps_view: {
-    paddingTop: 20,
-    paddingBottom: 30,
+    paddingVertical: 30,
     paddingHorizontal: 10,
   },
   steps_btn: {
@@ -261,7 +292,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginTop: 15,
+    marginTop: 20,
   },
   step_circle: {
     backgroundColor: '#414BB2',
@@ -288,6 +319,9 @@ const styles = StyleSheet.create({
     flex: 1,
     height: 5,
     backgroundColor: '#d9dbf0',
+  },
+  disabled: {
+    backgroundColor: '#737373',
   },
 });
 
